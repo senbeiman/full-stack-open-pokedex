5a67f9d6b764e26d16c092421665aa3f2b2b33bf
senbeiman <1000bei10000@gmail.com>
2021-01-18 22:02:33 +0900

    test build failure

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
